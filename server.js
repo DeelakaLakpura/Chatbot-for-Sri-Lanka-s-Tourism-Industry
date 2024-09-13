494599b974c7807b450dc29f1ae5dac0ffdad706
Deelaka Lakpura <81320954+DeelakaLakpura@users.noreply.github.com>
2024-09-13 10:41:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const genAI = new GoogleGenerativeAI('PATH_TO_GEMINI_API_KEY');
+const genAI = new GoogleGenerativeAI('AIzaSyCmvZmp1wjzLHqA-Uzx16kalkriSrVJEmM');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
